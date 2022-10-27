f9d55276c0505de3ea321a20d2efee5b0cc36759
Alonso Moreno <jesus.moreno@ucr.ac.cr>
2022-10-26 18:25:22 -0600

    first tests

--- coverage_demo/login/tests.py
+++ coverage_demo/login/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
+
 # Create your tests here.
+class EmailVerificationTest(TestCase):
+    def setUp(self) -> None:
+        pass
+
+    def test_email_verified(self):
+        self.assertTrue(True)
