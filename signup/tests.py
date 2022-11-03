8f8c2597e20475f34618126e6e2a89958f5c07b5
Alonso Moreno <jesus.moreno@ucr.ac.cr>
2022-11-02 18:41:35 -0600

    added unit tests for the create endpoint

--- signup/tests.py
+++ signup/tests.py
@@ -1,10 +1,27 @@
-from django.test import TestCase
+import json
+from django.contrib.auth.models import User
+from signup.models import KaizenUser
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APITestCase
+from rest_framework import status
+from signup.serializers import KaizenUserCreateSerializer, KaizenUserDetailSerializer, KaizenUserVerifySerializer
 
 
-# Create your tests here.
-class EmailVerificationTest(TestCase):
-    def setUp(self) -> None:
-        pass
+class SignUpTestCase(APITestCase):
+    def test_signup_register(self):
+        data = {'username': 'Alonso', 'email': 'alonso.moreno@mailinator.com', 'password': 'qwerty'}
+        response = self.client.post('/api/create/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
-    def test_email_verified(self):
-        assert True == True
+    def test_signup_register_is_left_unverified(self):
+        data = {'username': 'Alonso', 'email': 'alonso.moreno@mailinator.com', 'password': 'qwerty'}
+        response = self.client.post('/api/create/', data)
+        print(response)
+        self.assertFalse(response.data.get('is_verified'))
+
+    def test_signup_register_verification_code_not_empty(self):
+        data = {'username': 'Alonso', 'email': 'alonso.moreno@mailinator.com', 'password': 'qwerty'}
+        response = self.client.post('/api/create/', data)
+        print(response)
+        self.assertNotEqual(response.data.get('verification_code'), None)
+
