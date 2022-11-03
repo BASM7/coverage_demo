1e02c665e9d9d9827c6b9cfe3d3fc1e02a43d53f
Luis Jiménez <57238307+Fonsii@users.noreply.github.com>
2022-11-02 19:46:03 -0600

    Update tests.py

--- signup/tests.py
+++ signup/tests.py
@@ -10,25 +10,23 @@ from signup.serializers import KaizenUserCreateSerializer, KaizenUserDetailSeria
 class SignUpTestCase(APITestCase):
     """Tests for the SignUp API"""
     pass
-
-    # def test_signup_register(self):
-    #     data = {'username': 'Alonso', 'email': 'alonso.moreno@mailinator.com', 'password': 'qwerty'}
-    #     response = self.client.post('/api/create/', data)
-    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-    #     self.assertFalse(response.data.get('is_verified'))
-    #     self.assertNotEqual(response.data.get('verification_code'), None)
-    #
-    #
-    # def test_retrieve_verification(self):
-    #     data = {'username': 'Alonso', 'email': 'alonso.moreno@mailinator.com', 'password': 'qwerty'}
-    #     response = self.client.post('/api/create/', data)
-    #
-    #     verification_code = response.data.get('verification_code')
-    #
-    #     response = self.client.get('/api/verify/{}'.format(verification_code))
-    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
-    #     self.assertTrue(response.data.get('is_verified'))
-    #     self.assertEqual(response.data.get('verification_code'), 'EXPIRED')
-
-
-
+    
+    def test_signup_register(self):
+        data = {'username': 'Alonso', 'email': 'alonso.moreno@mailinator.com', 'password': 'qwerty'}
+        response = self.client.post('/api/create/', data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertFalse(response.data.get('is_verified'))
+        self.assertNotEqual(response.data.get('verification_code'), None)
+    
+    
+    def test_retrieve_verification(self):
+        data = {'username': 'Alonso', 'email': 'alonso.moreno@mailinator.com', 'password': 'qwerty'}
+        response = self.client.post('/api/create/', data)
+    
+        verification_code = response.data.get('verification_code')
+    
+        response = self.client.get('/api/verify/{}'.format(verification_code))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertTrue(response.data.get('is_verified'))
+        self.assertEqual(response.data.get('verification_code'), 'EXPIRED')
+        
